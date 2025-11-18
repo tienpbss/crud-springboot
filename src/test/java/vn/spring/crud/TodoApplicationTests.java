fe6c9edd7c176957fb0c2a0fd48fc29d23966f6c
tienpbss <tienphung.cn07@gmail.com>
2025-11-18 09:52:10 +0700

    add test profile

--- src/test/java/vn/spring/crud/TodoApplicationTests.java
+++ src/test/java/vn/spring/crud/TodoApplicationTests.java
@@ -1,13 +1,21 @@
 package vn.spring.crud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@IntegrationTest
 class TodoApplicationTests {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
 
 	@Test
 	void contextLoads() {
+        assertEquals("todo-test", applicationName);
 	}
 
 }
--- src/test/java/vn/spring/crud/controller/UserControllerIT.java
+++ src/test/java/vn/spring/crud/controller/UserControllerIT.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import vn.spring.crud.IntegrationTest;
 import vn.spring.crud.entity.User;
 import vn.spring.crud.repository.UserRepository;
 
@@ -17,8 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@IntegrationTest
 @AutoConfigureMockMvc
 public class UserControllerIT {
     @Autowired
