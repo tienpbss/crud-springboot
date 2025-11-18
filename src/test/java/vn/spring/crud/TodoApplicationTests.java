ad9063df4c6d6c8cd67378d839b3f85636f5d584
tienpbss <tienphung.cn07@gmail.com>
2025-11-18 12:54:33 +0700

    remove unused import

--- src/test/java/vn/spring/crud/TodoApplicationTests.java
+++ src/test/java/vn/spring/crud/TodoApplicationTests.java
@@ -2,10 +2,8 @@ package vn.spring.crud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IntegrationTest
 class TodoApplicationTests {
